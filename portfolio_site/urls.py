7b6399333063d81d1ac94f4cb0962af739083531
SQuinones <78192383+SQuinones@users.noreply.github.com>
2022-01-07 20:00:24 -0500

    adding some changes

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -5,6 +5,6 @@ from quote_generator import views
 urlpatterns = [
     
     path('', views.index, name = 'index'),
-    path('about/',views.about,name = 'about'),
+    # path('about/',views.about,name = 'about'),
     
 ]
